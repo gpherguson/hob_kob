f149f16c0e711465fc9ed5cbdc122e23dc4b8a1e
gpherguson <8si.greg@gmail.com>
2011-03-31 08:02:32 -0700

    change to use minitest

--- spec/yahoo_spec.rb
+++ spec/yahoo_spec.rb
@@ -38,37 +38,37 @@ class TestYahoo < MiniTest::Unit::TestCase
   def test_response_code
     yahoo = Yahoo.new('key','query')
     
-    assert_nil(yahoo.response_code)
+    # move down
   end
 
   def test_next_page
     yahoo = Yahoo.new('key','query')
 
-    assert_nil(yahoo.next_page)
+    # move down
   end
 
   def test_total_hits
     yahoo = Yahoo.new('key','query')
 
-    assert_nil(yahoo.total_hits)
+    # move down
   end
 
   def test_deep_hits
     yahoo = Yahoo.new('key','query')
 
-    assert_nil(yahoo.deep_hits)
+    # move down
   end
 
   def test_count
     yahoo = Yahoo.new('key','query')
 
-    assert_nil(yahoo.count)
+    # move down
   end
 
   def test_start_page
     yahoo = Yahoo.new('key','query')
 
-    assert_nil(yahoo.start_page)
+    # move down
   end
   
   def test_results
@@ -77,25 +77,32 @@ class TestYahoo < MiniTest::Unit::TestCase
 
     yahoo_results = yahoo.search
 
-    refute_empty(yahoo_results)
-    assert_kind_of(Array, yahoo_results)
-    
     refute_nil(yahoo.response_code)
     assert_kind_of(Fixnum, yahoo.response_code)
+    assert_equal(200, yahoo.response_code)
 
     refute_nil(yahoo.next_page)
     assert_kind_of(String, yahoo.next_page)
+    refute_empty(yahoo.next_page)
 
     refute_nil(yahoo.total_hits)
     assert_kind_of(Fixnum, yahoo.total_hits)
+    assert(yahoo.count >= 0)
 
     refute_nil(yahoo.deep_hits)
     assert_kind_of(Fixnum, yahoo.deep_hits)
+    assert(yahoo.count >= 0)
 
     refute_nil(yahoo.count)
     assert_kind_of(Fixnum, yahoo.count)
+    assert(yahoo.count >= 0)
 
     refute_nil(yahoo.start_page)
     assert_kind_of(Fixnum, yahoo.start_page)
+    assert_equal(0, yahoo.start_page)
+
+    # refute_empty(yahoo.results)
+    # assert_kind_of(Array, yahoo.results)
+    # refute_empty(yahoo.results)
   end
 end
