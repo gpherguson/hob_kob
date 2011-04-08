ad781c7a0da8a6dbbd830a214990f40eff97cae8
gpherguson <8si.greg@gmail.com>
2011-04-07 23:46:34 -0700

    fix for 2011

--- hob_backing_tracks_and_charts.rb
+++ hob_backing_tracks_and_charts.rb
@@ -1,31 +1,41 @@
 #!/usr/bin/env ruby -w
 
-require 'open-uri'
+require 'httpclient'
 require 'nokogiri'
 
-BASE_DIR = '/Users/greg/Music/HOB backing tracks/'
-URL = 'http://gc.guitarcenter.com/kingoftheblues/tracks/'
+BASE_DIR  = '/Users/greg/Music/HOB backing tracks/'
+URL       = 'http://gc.guitarcenter.com/kingoftheblues/tracks/index.cfm?track='
 
-doc = Nokogiri::HTML(open(URL))
+def get_file(href)
 
-doc.at('div.tracks').search('a').each do |a|
-  href = a['href']
-  
-  case href
-    
-  when /charts/
-    pdf = File.basename(href)
-    puts pdf
-    
-    if (!File.exists?(BASE_DIR + pdf))
-      File.open(BASE_DIR + pdf, 'wb') do |fo|
-        fo.print(open(URL + href).read)
-      end
+  fname = File.basename(href)
+  print fname
+
+  if (File.exists?(BASE_DIR + fname))
+
+    puts " exists, skipping."
+
+  else 
+
+    print " downloading..."
+
+    File.open(BASE_DIR + fname, 'wb') do |fo|
+
+      uri = URI.parse(URL).merge(href)
+      fo.print(HTTPClient.get_content(uri.to_s))
+
     end
-    
-  when /tracks/
-    # we don't care right now
-    puts a.text
+
+    puts " done"
+
   end
-  
+end
+
+1.upto(30) do |i|
+  doc = Nokogiri::HTML(HTTPClient.get_content(URL + i.to_s))
+
+  doc.search('a').select{ |a| a['href'][/\.(?:zip|pdf)$/] }.each do |a|
+    get_file(a['href'])
+  end
+
 end
